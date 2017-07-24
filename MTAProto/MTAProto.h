e2f9e0658e69c8262179b0e154b37be3afb620ef
Russell Stephens <russell.stephens@compass.com>
2017-07-24 09:48:17 -0400

    initial commit

---
+++ MTAProto/MTAProto.h
@@ -0,0 +1,19 @@
+//
+//  MTAProto.h
+//  MTAProto
+//
+//  Created by Russell Stephens on 7/24/17.
+//  Copyright © 2017 Russell Stephens. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MTAProto.
+FOUNDATION_EXPORT double MTAProtoVersionNumber;
+
+//! Project version string for MTAProto.
+FOUNDATION_EXPORT const unsigned char MTAProtoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MTAProto/PublicHeader.h>
+
+
